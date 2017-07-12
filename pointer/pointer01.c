50302c3c7b81636250d7ba7aa7f377194a26ba6a
wb-zkn182111 <wb-zkn182111@alibaba-inc.com>
2017-07-12 18:22:18 +0800

    修改代码

--- pointer/pointer01.c
+++ pointer/pointer01.c
@@ -38,7 +38,7 @@ void charAdd(const char *src){
     unsigned char *pEnd;
     const char *ppSrcs[2];
     int lens[2];
-    lens[0] = 7;
+    lens[0] = 10;
     ppSrcs[0] = src;
     printf("%s",pEnd);
     printf("%s\n","·É");
